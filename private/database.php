758a2d1cdcc25e140bc31e78159aca5321886504
宮部 陽向 <h.miyabe@miyabenoMacBook-Air.local>
2026-09-24 15:20:23 +0900

    データベース修正完了

--- private/database.php
+++ private/database.php
@@ -39,7 +39,7 @@ function saveContact(array $data): bool
     $name         = $data['user_name'] ?? '';
     $furigana     = $data['user_namefurigana'] ?? '';
     $email        = $data['user_email'] ?? '';
-    $gender = $_POST['gender'] ?? '';
+    $gender = $data['gender'] ?? '';
     $zip1         = $data['zip1'] ?? '';
     $zip2         = $data['zip2'] ?? '';
     $pref         = $data['pref'] ?? '';
